f8315e4f93c91bd3e948f97eedffc46a1af30371
ExcaliburSims <ximeonzilu@gmail.com>
2022-08-12 15:15:57 +0100

    feat: connect server to socket.io

--- index.js
+++ index.js
@@ -1,9 +1,25 @@
-const express = require('express');
-const app = express();
+const app = require('express')();
+const http = require('http').Server(app);
+const io = require('socket.io')(http);
+const path = require('path');
 const port = 8080;
 
+//app.use(express.static('./Views'))
+//app.use(app.static(path.join(__dirname, '/Views')));
+app.get('/message', (req, res) => {
+	res.sendFile(path.join(__dirname + '/Views/index.html'))
+})
+io.on("connection", (socket) => {
+	console.log('connected');
+	socket.on('disconnected', () => {
+		console.log('disconnected');
+	})
+	socket.on('chat message', (msg) => {
+		console.log('message reçu' + msg);
+	})
+});
 
 //SERVEUR
-app.listen(port, () => {
+http.listen(port, () => {
   console.log(`le serveur tourne au port ${port}`)
 })
