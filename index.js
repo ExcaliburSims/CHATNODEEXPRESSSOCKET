fce28339d78b5fbb8161ddb1c582f75628bd7d04
ExcaliburSims <ximeonzilu@gmail.com>
2022-08-14 15:43:58 +0100

    feat: create link in page

--- index.js
+++ index.js
@@ -1,13 +1,16 @@
-const app = require('express')();
+const express = require('express');
+const app = express();
 const http = require('http').Server(app);
 const io = require('socket.io')(http);
 const path = require('path');
 const port = 8080;
 
-//app.use(express.static('./Views'))
+app.use(express.static('Public'))
 //app.use(app.static(path.join(__dirname, '/Views')));
 app.get('/message', (req, res) => {
-	res.sendFile(path.join(__dirname + '/Views/index.html'))
+	//console.log(res.setHeader('content-type', 'text/plain'));
+	//res.setHeader('content-type', 'text/css')
+	res.status(200).sendFile('Views/index.html', {root: __dirname})
 })
 io.on("connection", (socket) => {
 	console.log('connected');
@@ -15,7 +18,7 @@ io.on("connection", (socket) => {
 		console.log('disconnected');
 	})
 	socket.on('chat message', (msg) => {
-		console.log('message reçu' + msg);
+		console.log('message reçu : ' + msg);
 	})
 });
 
