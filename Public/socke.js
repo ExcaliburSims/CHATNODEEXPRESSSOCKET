89e92f740ef7fcb12be8199512ae3363a7361250
ExcaliburSims <ximeonzilu@gmail.com>
2022-08-15 18:29:39 +0100

    feat: add js file

--- Public/socke.js
+++ Public/socke.js
@@ -1,27 +1,14 @@
 const socket = io();
-    $(() => {
-        getMessages();
-        $("#send").click(() => {
-            const message = {
-                name: $("#name").val(),
-                message: $("#message").val()
-            };
-            postMessage(message);
-        });
-    });
 
-    socket.on('message', addMessage);
+let send = () => {
+	const text = document.getElementById('message').value;
+	console.log(text);
+	socket.emit('chat message', text)
+}
 
-    function addMessage({ name, message }) {
-        $("#messages").append(`<h4>${name}</h4><p>${message}</p>`);
-    }
-
-    function getMessages() {
-        $.get('http://localhost:8080/messages', messages => {
-            messages.forEach(addMessage);
-        });
-    }
-
-    function postMessage(message) {
-        $.post('http://localhost:8080/message', message);
-    }
+let receive = (msg) => {
+	let li = document.createElement('li');
+	li.innerText = msg;
+	document.getElementById('messages-content').appendChild(li);
+}
+socket.on('chat message', receive);
