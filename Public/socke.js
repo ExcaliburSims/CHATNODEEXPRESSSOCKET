21ae8576aab5cd08cca5a68837ca573bbb3dcde7
ExcaliburSims <ximeonzilu@gmail.com>
2022-08-15 17:23:56 +0100

    feat: add js file

--- Public/socke.js
+++ Public/socke.js
@@ -1,1 +1,27 @@
-console.log('ok');
+const socket = io();
+    $(() => {
+        getMessages();
+        $("#send").click(() => {
+            const message = {
+                name: $("#name").val(),
+                message: $("#message").val()
+            };
+            postMessage(message);
+        });
+    });
+
+    socket.on('message', addMessage);
+
+    function addMessage({ name, message }) {
+        $("#messages").append(`<h4>${name}</h4><p>${message}</p>`);
+    }
+
+    function getMessages() {
+        $.get('http://localhost:8080/messages', messages => {
+            messages.forEach(addMessage);
+        });
+    }
+
+    function postMessage(message) {
+        $.post('http://localhost:8080/message', message);
+    }
