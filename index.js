2dbc13b908e6bc96112d6d21fc794f8eb1135f2c
ExcaliburSims <ximeonzilu@gmail.com>
2022-08-17 07:29:07 +0100

    feat: socket listen client

--- index.js
+++ index.js
@@ -1,43 +1,27 @@
 const express = require('express');
-/*const app = express();
-const http = require('http').Server(app);
-const io = require('socket.io')(http);
-//const port = 8080;
-
-//middleware to read css, image and js page
-
-app.use(express.static('Public'))
-app.get('/message', (req, res) => {
-	res.status(200).sendFile('Views/index.html', {root: __dirname})
-})
-io.on("connection", (socket) => {
-	console.log('connected');
-	socket.on('disconnected', () => {
-		console.log('disconnected');
-	})
-	socket.on('chat message', (msg) => {
-		console.log('message reçu : ' + msg);
-		io.emit('chat message', msg)
-	})
-}); */
 const app = express()
 const server = require('http').createServer(app)
+const io = require('socket.io')(server)
+const path = require('path')
 
 // just to test the server
 
+//middleware to read css, image and js page
 app.use(express.static('Public'))
+
 app.get('/message', (req, res) => {
 	res.status(200).sendFile('Views/index.html', {root: __dirname})
 })
+io.on('connection', socket => {
+	console.log('CONNECTED')
+	socket.on('chat message', (msg) => {
+		console.log('message reçu : ' + msg);
+		io.emit('chat', msg)
+	})
+})
 
+//SERVEUR
 server.listen(8080, () => {
 	const {port} = server.address();
   console.log(`Server running on port: ${port}`)
 })
-
-//SERVEUR
-
-/* const server = http.listen(8080, () => {
-    const {port} = server.address();
-    console.log(`Listening on port ${port}`);
-}); */
